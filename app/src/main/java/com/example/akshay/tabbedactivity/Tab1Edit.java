05ff5f69bc7c533b3e1ca3148386099f72748057
litelo <akshaymalav2017@gmail.com>
2019-02-10 09:26:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/akshay/tabbedactivity/Tab1Edit.java
@@ -0,0 +1,16 @@
+package com.example.akshay.tabbedactivity;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class Tab1Edit extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1edit, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/akshay/tabbedactivity/Tab3Results.java
@@ -0,0 +1,17 @@
+package com.example.akshay.tabbedactivity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Results extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3results, container, false);
+
+        return rootView;
+    }
+}
